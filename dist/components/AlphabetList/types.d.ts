0fa481428e71389d302c98ac5d7d3842afb71e8c
Kinzey-R <kinzey@qsstudio.co>
2022-11-22 13:44:48 +1100

    run build

--- dist/components/AlphabetList/types.d.ts
+++ dist/components/AlphabetList/types.d.ts
@@ -3,7 +3,7 @@ import { SectionListData, SectionListProps, TextStyle, ViewStyle } from "react-n
 export interface IData {
     value: string;
     key: string;
-    listData: {
+    listData?: {
         name: string;
         id: number;
     }[];
